ef2d123ffe7e3c8dd1b4b589f7a6a7e31128a621
pratikesh <pratikesh.axzora@gmail.com>
2022-02-18 17:48:06 +0530

    APIs for designation

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,15 @@ import { ConfigModule } from '@nestjs/config';
 import { configValidationSchema } from './config.schema';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './module/users/users.module';
+import { DesignationModule } from './module/designation/designation.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     AuthModule,
     UsersModule,
-    DatabaseModule
+    DatabaseModule,
+    DesignationModule
   ],
 })
-export class AppModule {}
+export class AppModule { }
