607664c03d91e3ffd1c9c7bfaa42f426fecbfa63
Osborne D'Silva <osborne.axzora@gmail.com>
2022-02-18 12:47:56 +0530

    Added Employee Module to App Module file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
-import { configValidationSchema } from './config.schema';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './module/users/users.module';
+import { EmployeesModule } from './module/employees/employees.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     AuthModule,
     UsersModule,
-    DatabaseModule
+    DatabaseModule,
+    EmployeesModule
   ],
 })
 export class AppModule {}
