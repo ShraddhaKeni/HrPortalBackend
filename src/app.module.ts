d6132fd7860cefb58574c256c2f2914f4aa98f16
Pratikesh Mandrekar <pratikesh.axzora@gmail.com>
2022-02-21 16:41:21 +0530

    app module file

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { CompaniesModule } from './module/companies/companies.module';
 import { CountriesModule } from './module/countries/countries.module';
 import { StatesModule } from './module/states/states.module';
 import { CitiesModule } from './module/cities/cities.module';
+import { UserDocsModule } from './module/user-docs/user-docs.module';
 
 @Module({
   imports: [
@@ -31,7 +32,8 @@ import { CitiesModule } from './module/cities/cities.module';
     ReportingToModule,
     SalaryModule,
     DocumentTypeModule,
-    EmployeesModule
+    EmployeesModule,
+    UserDocsModule,
   ],
 })
 export class AppModule { }
