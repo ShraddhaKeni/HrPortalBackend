df2404a419512453664d66144897799344b852dc
Vivek Rane <vivek.axzora@gmail.com>
2022-02-21 15:31:20 +0530

    added swagger api

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,22 @@ import { AppModule } from './app.module';
 import { TransformInterceptor } from './transform.interceptor';
 import { Logger } from '@nestjs/common';
 import * as dotenv from 'dotenv';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   dotenv.config();
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('HR Portal')
+    .setDescription('HR Portal API Documentation')
+    .setVersion('1.0')
+    .addTag('hr-portal')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
